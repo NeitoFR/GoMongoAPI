c5fc3f003b2108345855cd3df6373bac71d10675
Wille Houm <willem.houm@gmail.com>
2018-12-10 23:14:35 +0100

    Get All ugly but OK

--- app.go
+++ app.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
-func GetPassports(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet")
+func GetAllPeoplesRoute(w http.ResponseWriter, r *http.Request) {
+	log.Println("Getting all peoples")
+	res := _getAllPeople()
+	log.Println("Got " + strconv.Itoa(len(res)) + " people")
+	// json_res := json.Marshal(res[0])
+	fmt.Fprintln(w, res)
 }
 
+func GetPeopleByName() {
+
+}
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/passports", GetPassports).Methods("GET")
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatal(err)
-	}
+	godotenv.Load("./mongo.env")
+	_initMongoConnection(mongoInfo{os.Getenv("mongo_url"), os.Getenv("mongo_db"), os.Getenv("mongo_col")})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/peoples", GetAllPeoplesRoute).Methods("GET")
+
+	log.Println("Listening on port : " + os.Getenv("port"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("port"), router))
 }
