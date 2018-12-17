0eaa28f00c696fa28227729d6ebf9f58dac5cce4
Wille Houm <willem.houm@gmail.com>
2018-12-17 22:54:36 +0100

    Get mongodbpassport clean OK

--- go_api/app/api.go
+++ go_api/app/api.go
@@ -11,18 +11,38 @@ import (
 )
 
 func GetAllRoute(w http.ResponseWriter, r *http.Request) {
-	log.Println("Getting all passports")
-	res := _getAll()
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Error parsing URL", err)
+		panic(err)
+	}
+	res, err := _getPassports(r.Form)
 
-	fmt.Fprintf(w, res)
+	if err != nil {
+		log.Println("Error getting passport", err)
+		fmt.Fprintln(w, err)
+	}
+	fmt.Fprintln(w, res)
 }
 
+func test(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	m := r.Form
+	log.Println(m)
+
+	keys := make([]string, len(r.Form))
+	for key, _ := range r.Form {
+		keys = append(keys, key)
+	}
+	log.Println(keys)
+}
 func main() {
 	godotenv.Load("./mongo.env")
 	_initMongoConnection(mongoInfo{os.Getenv("mongo_url"), os.Getenv("mongo_db"), os.Getenv("mongo_col")})
 
 	router := mux.NewRouter()
 	router.HandleFunc("/passports", GetAllRoute).Methods("GET")
+	router.HandleFunc("/test", test).Methods("GET")
 
 	log.Println("Listening on port : " + os.Getenv("port"))
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("port"), router))
--- go_api/app/mongo_helper.go
+++ go_api/app/mongo_helper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net/url"
 
 	"gopkg.in/mgo.v2/bson"
 
@@ -28,20 +29,30 @@ func _initMongoConnection(info mongoInfo) {
 	log.Println("Connection to the mongo instance OK : \nMongo URL : " + info.url + "\nDatabase Name :" + info.db_name + "\nCollection Name : " + info.col_name)
 }
 
-func _getAll() string {
-	var data []bson.M
+func _getPassports(a url.Values) (string, error) {
+	var data bson.M
+	var err error = nil
 
-	err := col.Find(bson.M{}).All(&data)
+	if len(a) > 0 {
+		keys := make([]string, len(a))
+		for key, _ := range a {
+			keys = append(keys, key)
+		}
+		log.Println(keys, a, len(a))
+		// #TODO Parse url.Values with the $and operator and do a filtered Find() query
+		err = col.Find(bson.M{}).One(&data)
+	} else {
+		err = col.Find(bson.M{}).One(&data)
+	}
 	if err != nil {
 		log.Println("Error getting passports", err)
-		panic(err)
+		return "", err
 	}
-	log.Println(data)
-	res_json, err := json.Marshal(data)
+	toByte, err := json.Marshal(data)
 	if err != nil {
 		log.Println("Error converting bson to byte[] ", err)
-		panic(err)
+		return "", err
 	}
 
-	return string(res_json[:len(res_json)])
+	return string(toByte[:len(toByte)]), err
 }
